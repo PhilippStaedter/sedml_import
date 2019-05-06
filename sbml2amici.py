cd6a17882eb6562adaa5064537ec71d9fc6b0486
PhilippStaedter <philippstaedter@kabelmail.de>
2019-05-06 16:20:05 +0200

    python script for the sbml to amici import ---- updated

--- sbml2amici.py
+++ sbml2amici.py
@@ -7,18 +7,23 @@ import sys
 import numpy as np
 import logging
 import shutil
+import pandas as pd
 
+
+# create .tsv file
+tsv_table = pd.DataFrame(columns=['id', 'states', 'reactions', 'error_message'])      # index=range() can be left out
 
 # important paths
 models_path = "../sbml2amici/amici_models"
-base_path = "/sedml_models"
+models_base_path = "../sbml2amici"
+base_path = "./sedml_models"
 
 
 # create directory for all future amici models
 if not os.path.exists(models_path):
     os.makedirs(models_path)
 
-# Create logger object
+# create logger object
 logger = logging.getLogger()
 
 # initialize the log settings
@@ -26,15 +31,37 @@ logging.basicConfig(filename='all_logs',level=logging.DEBUG)
 
 # list of all directories + SBML files
 list_directory = os.listdir(base_path)
+list_directory = sorted(list_directory)
+
+# set row-counter for .tsv file
+counter = 0
 
 for models in list_directory:
     list_files = os.listdir(base_path + '/' + models + '/sbml_models')
+    list_files = sorted(list_files)
     for files in list_files:
         sbml_file = base_path + '/' + models + '/sbml_models/' + files
         model_name, other_stuff = files.split(".",1)
         model_output_dir = models_path + '/' + models + '/' + model_name
 
         try:
+            ## entry = {'id': None, 'states': None, 'reactions': None, 'parameters': None, 'error_message': None}
+            # Append additional row in .tsv file
+            tsv_table = tsv_table.append({}, ignore_index=True)
+
+            # name
+            tsv_table.loc[counter].id = '{' + models + '}' + '_' + '{' + files + '}'
+            ## entry['id'] = ...
+            # read accompanying sbml file
+            file = libsbml.readSBML(sbml_file)
+            all_properties = file.getModel()
+            num_states = len(all_properties.getListOfSpecies())
+            num_reactions = len(all_properties.getListOfReactions())
+
+            # Fill in .tsv file
+            tsv_table.loc[counter].states = num_states
+            tsv_table.loc[counter].reactions = num_reactions
+
             # Create SBML importer
             sbml_importer = amici.SbmlImporter(sbml_file)
 
@@ -43,16 +70,35 @@ for models in list_directory:
                         model_output_dir,
                         verbose=False)
 
-        except:
-            print(sys.exc_info()[0])
+            # Write 'OK' in 'error_message' coloumn
+            tsv_table.loc[counter].error_message = 'OK'
+
+            # increase counter by 1
+            counter = counter + 1
+
+        except Exception as e:
+            error_info = str(e)
+            # error_info = sys.exc_info()[0]
+            print(error_info)
             logging.exception('Model failed: %s, %s', models, files)
             logging.info('\n')
-            #logging.exception(str(Exception))
-        #except Exception as e:
-         #   logging.exeption(str(e), exc_info=True)
-            #continue
+
+            # Write the error message in 'error_message' coloumn
+            tsv_table.loc[counter].error_message = error_info
+            ## tsv_table.append(entry)
+            # increase counter by 1
+            counter = counter + 1
+
+            # logging.exception(str(Exception))
+            # except Exception as e:
+            # logging.exeption(str(e), exc_info=True)
+            # continue
+
+# print tsv_table + save it
+print(tsv_table)
+tsv_table.to_csv(path_or_buf=models_base_path + '/table.tsv', sep='\t', index=True)
 
 # copy file 'all_logs' in new directory 'sbml2amici'
-old_path = '/all_logs'
-new_path = models_path + '/all_logs'
+old_path = './all_logs'
+new_path = models_base_path + '/all_logs'
 shutil.move(old_path, new_path)
