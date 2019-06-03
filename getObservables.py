1cc9bc11b3002008cab8670087475c5a6eb15e1a
PhilippStaedter <philippstaedter@kabelmail.de>
2019-06-03 16:42:04 +0200

    updated version of the python script that writes observables in a new SBML file

--- getObservables.py
+++ getObservables.py
@@ -2,41 +2,73 @@
 
 import libsedml
 import libsbml
+import sys
+import os
+import importlib
 
-def getObservables(iSEDML, iSBML):
 
-    # important paths
-    sedml_path = './sedml_models/' + iSEDML + '/' + iSBML + '.sedml'
-    sbml_path = './sedml_models/' + iSEDML + '/sbml_models/' + iSBML + '.sbml'
+iSEDML = 'bachmann2011'
+iSBML = 'bachmann'
 
-    # read in sedml file
-    sedml_file = libsedml.readSedML(sedml_path)
+# def getObservables(iSEDML, iSBML):
 
-    # get important values
-    obsFormula = libsedml.formulaToString(sedml_file.getDataGenerator(0).getMath())             # has to be autmomatist
-    obsId = sedml_file.getDataGenerator(0).getId()                                              # iCount instead of 0
-    SBML_model_Id = obsId.split('_',1)
-    obsId = 'observable_' + obsId
+# important paths
+sedml_path = './sedml_models/' + iSEDML + '/' + iSEDML + '.sedml'
+sbml_path = './sedml_models/' + iSEDML + '/sbml_models/' + iSBML + '.sbml'
+new_sbml_path = './sedml_models/' + iSEDML + '/sbml_models/' + iSBML + '_with_observabels.sbml'
 
-    # get list of parameters
-    list_par = sedml_file.getParameters()
+# read in sedml file
+sedml_file = libsedml.readSedML(sedml_path)
 
-    # read in sbml model
-    sbml_file = libsbml.readSBML(sbml_path)
+# get number of tasks and observables
+num_task = sedml_file.getNumTasks()
+num_obs = sedml_file.getNumDataGenerators()
 
-    assignemtRule = sbml_file.createAssignmentRule()
-    assignemtRule.setId(obsId)
-    assignemtRule.setVariable(obsId)
-    assignemtRule.setFormula(obsFormula)
+for iTask in range(0, num_task):
+    task_id = sedml_file.getTask(iTask).getId()
 
-    parameter = sbml_file.createParameter()
-    parameter.setId(obsId)
-    parameter.setConstant(False)
+    for iObservable in range(0, num_obs):
+        # get important formula
+        obs_Formula = libsedml.formulaToString(sedml_file.getDataGenerator(iObservable).getMath())             # has to be autmomatist
+        obs_Id = sedml_file.getDataGenerator(iObservable).getId()                                              # iCount instead of 0
+        SBML_model_Id,Observable_Id = obs_Id.split('_',1)
+        new_obs_Id = 'observable_' + Observable_Id
 
-    for iPar in range(0, len(list_par)):
-        parameter = libsbml.createParameter()
-        parameter.setId(iPar)
-        parameter.setConstant(False)
-        parameter.setValue(iPar)
+        # get list of parameters
+        list_par = []
+        num_par = sedml_file.getDataGenerator(iObservable).getNumParameters()
+        if num_par == 0:
+            print(obs_Id + ' has no parameters as observables!')
+        else:
+            for iCount in range(0, num_par):
+                list_par.append(sedml_file.getDataGenerator(iObservable).getParameter(1).getId())
 
-    writeSBMLToFile(sbml_model_no_observables,sbml_model_with_observables)
+        # get task reference
+        task_ref = sedml_file.getDataGenerator(iObservable).getVariable(0).getTaskReference()
+
+        # read in sbml model
+        sbml_file = libsbml.readSBML(sbml_path)
+
+        if task_id == task_ref:
+            # create formula
+            assignmentRule = sedml_file.createDataGenerator()
+            # assignmentRule = sbml_file.createAssignmentRule()     # not availabale
+            assignmentRule.setId(new_obs_Id)
+            assignmentRule.createVariable()
+            #assignmentRule.setVariable(new_obs_Id)                 # not available
+            assignmentRule.setFormula(obs_Formula)
+
+            # create parameter to formula for observables
+            obs_parameter = sbml_file.createParameter()
+            obs_parameter.setId(new_obs_Id)
+            obs_parameter.setConstant(False)
+
+            # create parameter to formula for parameters
+            for iCount in range(0, num_par):
+                iPar = list_par[iCount]
+                parameter = libsbml.createParameter()
+                parameter.setId(iPar)
+                parameter.setConstant(False)                        # not available
+                parameter.setValue(iPar)                            # not available
+
+    libsbml.writeSBMLToFile(sbml_path,new_sbml_path)
