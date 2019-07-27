4ec77759e200eb12c66f0e4ed2ea98bb3575ea8b
PhilippStaedter <philippstaedter@kabelmail.de>
2019-07-28 00:31:56 +0200

    updated script to decompose formula to determine the kinetic law easier

--- decomposeFormula.py
+++ decomposeFormula.py
@@ -1,54 +1,126 @@
 # script to decompose formula in smaller compartments:
-
+# not robust against more special cases!!!
 
 from opposingBracket import *
 
-formula = '(AA + DD) * V * Y + ABCABC * B - C * (ZZZZ + R) * X'
+#formula = 'pow(FFF, 4) / (A() + Q) + D / A * (AA + DD) * V * Y + ABCABC * B - C * (ZZZZ + R) * pow(X, 3)'
 
-#def decomposition(formula):
+def decomposition(formula):
 
-list_of_compartments = []
+    list_of_compartments = []
 
-iter_object = len(formula)
-iElement = 0
-while iElement < iter_object:
+    iter_object = len(formula)
+    iElement = 0
+    while iElement < iter_object:
 
-    # case 1: brackets  ->  ends after closing bracket
-    if formula[iElement] == '(':
-        matching_index = getIndex(formula, iElement)
-        if formula[matching_index + 2] == '/':
-            a = 4
+        # case 1: brackets                                                  # no case for '+' or '-' necessary! (otherwise brackets would be unnecessary)
+        if formula[iElement] == '(':
+            matching_index = getIndex(formula, iElement)
+            if matching_index + 2 < iter_object:
+                if formula[matching_index + 2] == '/':
 
-        elif formula[matching_index + 2] == '*':
-            list_of_compartments.append(formula[iElement: next_white_space])
-            formula = formula[next_white_space + 3 :]
-            iter_object = len(formula)
+                    # everything all over again
+                    if formula[matching_index + 4] == '(':
+                        matching_index = getIndex(formula, matching_index + 4)
+                        list_of_compartments.append(formula[iElement : matching_index + 1])
+                        formula = formula[matching_index + 4 :]
+                        iter_object = len(formula)
 
-        else:
-            print('This case must not happen ... Something is wrong!')
+                    else:
+                        next_white_space = formula.find(' ', matching_index + 4)
+                        list_of_compartments.append(formula[iElement: next_white_space])
+                        formula = formula[next_white_space + 3:]
+                        iter_object = len(formula)
 
+                elif formula[matching_index + 2] == '*':
+                    list_of_compartments.append(formula[iElement : matching_index + 1])
+                    formula = formula[matching_index + 4 :]
+                    iter_object = len(formula)
 
-    # case 2: just one species
-    else:
-        next_white_space = formula.find(' ')
-        if formula[next_white_space + 1] == '+' or formula[next_white_space + 1] == '-':
-            list_of_compartments.append(formula[iElement : next_white_space])
-            formula = formula[next_white_space + 3 :]
-            iter_object = len(formula)
+                else:
+                    print('This case 1 must not happen ... Something is wrong!')
+            else:
+                list_of_compartments.append(formula[iElement : len(formula)])
+                # get to return value
+                iElement = iter_object
 
-        elif formula[next_white_space + 1] == '*' and formula[next_white_space + 3] != '(':
-            list_of_compartments.append(formula[iElement : next_white_space])
-            formula = formula[next_white_space + 3 :]
-            iter_object = len(formula)
+        # (special) case 2: starts with 'pow(' without brackets
+        elif formula[iElement : iElement + 4] == 'pow(':
+            matching_index = getIndex(formula, iElement + 3)
+            if matching_index + 2 < iter_object:
+                if formula[matching_index + 2] == '+' or formula[matching_index + 2] == '-' or formula[matching_index + 2] == '*':
+                    list_of_compartments.append(formula[iElement : matching_index + 1])
+                    formula = formula[matching_index + 4:]
+                    iter_object = len(formula)
 
-        elif formula[next_white_space + 1] == '*' and formula[next_white_space + 3] == '(':
-            matching_index = getIndex(formula, next_white_space + 3)
-            if formula[matching_index + 2] == '/':
+                elif formula[matching_index + 2] == '/':
 
-                # repeat whole text as before
-                a = 4
+                    # desaster
+                    if formula[matching_index + 4] == '(':
+                        matching_index = getIndex(formula, matching_index + 4)
+                        list_of_compartments.append(formula[iElement : matching_index + 1])
+                        formula = formula[matching_index + 4 :]
+                        iter_object = len(formula)
+
+                    else:
+                        next_white_space = formula.find(' ', matching_index + 4)
+                        list_of_compartments.append(formula[iElement: next_white_space])
+                        formula = formula[next_white_space + 3:]
+                        iter_object = len(formula)
+
+                else:
+                    print('This case 2 must not happen ... Something is wrong!')
 
             else:
-                list_of_compartments.append(formula[iElement : matching_index + 1])
-                formula = formula[matching_index + 3:]
-                iter_object = len(formula)
+                list_of_compartments.append(formula[iElement: len(formula)])
+                # get to return value
+                iElement = iter_object
+
+        # case 3: no brackets, no special cases
+        elif formula[iElement] != '(' and formula[iElement : iElement + 4] != 'pow(':
+            next_white_space = formula.find(' ')
+            if next_white_space != -1:
+                if formula[next_white_space + 1] == '+' or formula[next_white_space + 1] == '-' or formula[next_white_space + 1] == '*':
+                    list_of_compartments.append(formula[iElement : next_white_space])
+                    formula = formula[next_white_space + 3 :]
+                    iter_object = len(formula)
+
+                elif formula[next_white_space + 1] == '/':
+
+                    # everything all over again
+                    if formula[next_white_space + 3] == '(':
+                        matching_index = getIndex(formula, next_white_space + 3)
+                        list_of_compartments.append(formula[iElement: matching_index + 1])
+                        formula = formula[matching_index + 4:]
+                        iter_object = len(formula)
+
+                    else:
+                        next_white_space = formula.find(' ', next_white_space + 3)
+                        list_of_compartments.append(formula[iElement: next_white_space])
+                        formula = formula[next_white_space + 3:]
+                        iter_object = len(formula)
+
+                else:
+                    print('This case 3 must not happen ... Something is wrong!')
+
+            else:
+                list_of_compartments.append(formula[iElement : len(formula)])
+                # get to return value
+                iElement = iter_object
+
+    return list_of_compartments
+
+
+
+'''     
+        elif formula[next_white_space + 1] == '*':
+            list_of_compartments.append(formula[iElement : next_white_space])
+            formula = formula[next_white_space + 3 :]
+            iter_object = len(formula)
+            
+        elif formula[next_white_space + 1] == '*' and formula[next_white_space + 3] == '(':
+            matching_index = getIndex(formula, next_white_space + 3)
+            list_of_compartments.append(formula[iElement : matching_index + 1])
+            formula = formula[matching_index + 3:]
+            iter_object = len(formula)
+'''
