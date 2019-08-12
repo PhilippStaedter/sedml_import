193408eeadd4fd53bf1e76fccbcd57347e99a220
PhilippStaedter <philippstaedter@kabelmail.de>
2019-08-12 17:10:22 +0200

    updated script to add new models to the benchmark collection

--- simulatePetab.py
+++ simulatePetab.py
@@ -8,8 +8,13 @@ import numpy as np
 import pandas as pd
 import pypesto.visualize
 
+
+iModel = 'kouril2017_fig2b'
+n_starts = 5
+
+
 # important paths
-model_base_path = './sedml_models/perelson1996_fig1b_top'
+model_base_path = './sedml_models/' + iModel
 
 # manage petab problem
 petab_problem = petab.Problem.from_folder(model_base_path)      #'./sedml_models/perelson1996_fig1b_top/experimental_data_rearranged')
@@ -34,18 +39,18 @@ optimizer = pypesto.ScipyOptimizer()
 problem = importer.create_problem(obj)
 engine = pypesto.SingleCoreEngine()
 # engine = pypesto.MultiProcessEngine()
-result = pypesto.minimize(problem=problem, optimizer=optimizer, n_starts=5, engine=engine)
+result = pypesto.minimize(problem=problem, optimizer=optimizer, n_starts=n_starts, engine=engine)
 
 # visualize
 ref = pypesto.visualize.create_references(x=petab_problem.x_nominal, fval=obj(petab_problem.x_nominal))
 pypesto.visualize.waterfall(result, reference=ref, scale_y='lin')
 fig = plt.gcf()
 fig.set_size_inches(18.5, 10.5)
-plt.savefig(model_base_path + '/waterfall_estimation.pdf')
+plt.savefig(model_base_path + '/waterfall_estimation_' + str(n_starts) + '_merrors.pdf')
 pypesto.visualize.parameters(result, reference=ref)
 fig = plt.gcf()
 fig.set_size_inches(18.5, 10.5)
-plt.savefig(model_base_path + '/waterfall_parameter.pdf')
+plt.savefig(model_base_path + '/waterfall_parameter_' + str(n_starts) + '_merrors.pdf')
 print(result.optimize_result.get_for_key('fval'))
 
 # simulated data
@@ -59,5 +64,5 @@ plt.scatter(petab_problem.measurement_df['measurement'], df['measurement'])
 # save all plots and data frame in same folder
 fig = plt.gcf()
 fig.set_size_inches(18.5, 10.5)
-plt.savefig(model_base_path + '/measurement.pdf')
-df.to_csv(model_base_path + '/simulatedData_perelson1996_fig1b_top.tsv', sep='\t', index=False)
+plt.savefig(model_base_path + '/measurement_' + str(n_starts) + '_merrors.pdf')
+df.to_csv(model_base_path + '/simulatedData_' + iModel + '_' + str(n_starts) + '_merrors.tsv', sep='\t', index=False)
