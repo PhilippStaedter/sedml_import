70869385eb2a141c39c24926d10321364101224e
PhilippStaedter <philippstaedter@kabelmail.de>
2019-08-12 13:17:55 +0200

    updated script for a parameter estimation

--- simulatePetab.py
+++ simulatePetab.py
@@ -5,14 +5,14 @@ import petab
 import amici
 import matplotlib.pyplot as plt
 import numpy as np
-
+import pandas as pd
+import pypesto.visualize
 
 # important paths
-model_base_path = '../benchmark-models/hackathon_contributions_new_data_format/Boehm_JProteomeRes2014'
-other_one = './sedml_models/perelson1996_fig1b_top'
+model_base_path = './sedml_models/perelson1996_fig1b_top'
 
 # manage petab problem
-petab_problem = petab.Problem.from_folder(other_one)      #'./sedml_models/perelson1996_fig1b_top/experimental_data_rearranged')
+petab_problem = petab.Problem.from_folder(model_base_path)      #'./sedml_models/perelson1996_fig1b_top/experimental_data_rearranged')
 petab_problem.get_optimization_to_simulation_parameter_mapping()
 
 # import model to amici
@@ -29,4 +29,35 @@ obj = importer.create_objective()
 print("Nominal parameter values:\n", petab_problem.x_nominal)
 print(obj(petab_problem.x_nominal))
 
-a = 4
+# run optimization
+optimizer = pypesto.ScipyOptimizer()
+problem = importer.create_problem(obj)
+engine = pypesto.SingleCoreEngine()
+# engine = pypesto.MultiProcessEngine()
+result = pypesto.minimize(problem=problem, optimizer=optimizer, n_starts=5, engine=engine)
+
+# visualize
+ref = pypesto.visualize.create_references(x=petab_problem.x_nominal, fval=obj(petab_problem.x_nominal))
+pypesto.visualize.waterfall(result, reference=ref, scale_y='lin')
+fig = plt.gcf()
+fig.set_size_inches(18.5, 10.5)
+plt.savefig(model_base_path + '/waterfall_estimation.pdf')
+pypesto.visualize.parameters(result, reference=ref)
+fig = plt.gcf()
+fig.set_size_inches(18.5, 10.5)
+plt.savefig(model_base_path + '/waterfall_parameter.pdf')
+print(result.optimize_result.get_for_key('fval'))
+
+# simulated data
+rdatas = obj(result.optimize_result.get_for_key('x')[0], return_dict=True)['rdatas']
+df = importer.rdatas_to_measurement_df(rdatas)
+plt.xlabel("Experiment")
+plt.ylabel("Simulation")
+plt.scatter(petab_problem.measurement_df['measurement'], df['measurement'])
+#plt.show()
+
+# save all plots and data frame in same folder
+fig = plt.gcf()
+fig.set_size_inches(18.5, 10.5)
+plt.savefig(model_base_path + '/measurement.pdf')
+df.to_csv(model_base_path + '/simulatedData_perelson1996_fig1b_top.tsv', sep='\t', index=False)
