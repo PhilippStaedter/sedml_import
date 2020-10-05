b325f01b6f2465c2e34b490e66229520ab09cb6a
PhilippStaedter <philippstaedter@kabelmail.de>
2020-10-05 12:27:47 +0200

    new python scripts due to review

---
+++ compareStateTrajectories_140settings_2.py
@@ -0,0 +1,149 @@
+# script 2 to compare state trajectories from JWS with trajectories of the simulation
+# => compares state trajectories
+
+# Attention:    boundary conditions are not being simulated by JWS!
+
+from execute_loadModels import *
+from JWS_changeValues import *
+from colourDataFrame import *
+import amici.plotting
+import numpy as np
+import matplotlib.pyplot as plt
+import libsbml
+import libsedml
+import time
+import statistics
+import pandas as pd
+import os
+import urllib.request
+import json
+import itertools
+import time
+
+
+def compStaTraj_JWS_2():
+    # upper and lower boundaries for the absolute and relative errors
+    AbsError_1 = range(-4,-3)
+    RelError_2 = range(-4,-3)
+
+    # create folder for all results
+    folder_path = './Whole_state_trajectory_comparison'
+    if not os.path.exists(folder_path):
+        print('Error: The folder Whole_state_trajectory_comparison should have been created by now!')
+        sys.exit()
+
+    # loop over all created folders containing the simulation data from JWS' built-in routine and Amici
+    list_simulation_data = sorted(os.listdir(folder_path))
+    for iSimDataGroup in list_simulation_data:
+        # get all hyperparameters
+        solAlg, nonLinSol, linSol, absTol, relTol = iSimDataGroup.split('_')
+
+        # iterate over all error combinations
+        for iAbsError in range(0, len(AbsError_1)):
+            for iRelError in range(0, len(RelError_2)):
+
+                if iAbsError != iRelError:
+                    continue
+
+                # set errors
+                abs_error = float('1e' + str(AbsError_1[iAbsError]))
+                rel_error = float('1e' + str(RelError_2[iRelError]))
+
+                # int2str
+                abs_str = '{:.0e}'.format(float(abs_error))
+                rel_str = '{:.0e}'.format(float(rel_error))
+
+                print(f"TOLERANCES: abs={abs_str} rel={rel_str}")
+
+                # set counter
+                counter = 0
+
+                # measure time needed for all models
+                start_time = time.time()
+
+                # iterate over all models
+                list_models_with_simulationData = sorted(os.listdir(folder_path + '/' + iSimDataGroup + '/json_files'))
+                for iMod in range(0, len(list_models_with_simulationData)):
+                    iModel = list_models_with_simulationData[iMod]
+                    #iModel = 'vanheerden2014_fig4-user'
+
+                    list_files = sorted(os.listdir(folder_path + '/' + iSimDataGroup + '/json_files/' + iModel))
+                    for iFile in list_files:
+                        print(f"    {iModel} :: {iFile}")
+
+                        # important paths
+                        old_json_save_path = folder_path + '/' + iSimDataGroup + '/json_files/' + iModel + '/' + iFile
+                        new_json_save_path = folder_path + '/' + f'{solAlg}_{nonLinSol}_{linSol}_{absTol}_{relTol}' \
+                                             + '/all_results_json_files/json_files_' + abs_str + '_' + rel_str + '/' + iModel + '/' + iFile
+                        BioModels_path = './BioModelsDatabase_models'
+
+                        if os.path.exists(BioModels_path + '/' + iModel):
+                            print('Model is still not part of JWS-database!')
+                        else:
+
+                            # create folder
+                            if not os.path.exists(new_json_save_path):
+                                os.makedirs(new_json_save_path)
+
+                            # open jws_simulation .csv file
+                            tsv_file = pd.read_csv(old_json_save_path + '/' + iFile + '_JWS_simulation.csv', sep='\t')
+
+                            # open model_simulation .csv file
+                            df_state_trajectory = pd.read_csv(old_json_save_path + '/' + iFile + '_model_simulation.csv', sep='\t')
+
+                            # columns names of .tsv file
+                            column_names = list(tsv_file.columns)
+                            column_names.remove('time')
+                            del tsv_file['time']
+
+                            # comparison
+                            amount_col = len(column_names)
+                            first_col = column_names[0]
+                            amount_row = len(df_state_trajectory[first_col])
+                            df_single_error = pd.DataFrame(columns=column_names, data=np.zeros((amount_row, amount_col)))
+                            df_trajectory_error = pd.DataFrame(columns=column_names, data=np.zeros((1, amount_col)))
+                            df_whole_error = pd.DataFrame(columns=['trajectories_match'], data=np.zeros((1, 1)))
+
+                            # single error
+                            for iCol in column_names:
+                                for iRow in range(0, amount_row):
+                                    rel_tol = abs((df_state_trajectory.at[iRow, iCol] - tsv_file.at[iRow,iCol]) / df_state_trajectory.at[iRow, iCol])
+                                    abs_tol = abs(df_state_trajectory.at[iRow, iCol] - tsv_file.at[iRow,iCol])
+                                    if rel_tol <= rel_error or abs_tol <= abs_error:
+                                        df_single_error.at[iRow, iCol] = 1
+                                    else:
+                                        df_single_error.at[iRow, iCol] = 0
+
+                            # trajectory error
+                            for iCol in column_names:
+                                if sum(df_single_error[iCol]) == amount_row:
+                                    df_trajectory_error.at[0, iCol] = 1
+                                else:
+                                    df_trajectory_error.at[0, iCol] = 0
+
+                            # whole error
+                            error_list = []
+                            for iCol in column_names:
+                                error_list.append(df_trajectory_error.at[0, iCol])
+                            if sum(error_list) == amount_col:
+                                df_whole_error.at[0, 'trajectories_match'] = 1
+                            else:
+                                df_whole_error.at[0, 'trajectories_match'] = 0
+
+                            # adjust counter
+                            if df_whole_error.at[0, 'trajectories_match'] == 1:
+                                print('matching state trajectory!')
+                                counter = counter + 1
+
+                            # save outcome
+                            df_single_error.to_csv(path_or_buf=new_json_save_path + '/single_error.csv', sep='\t', index=False)
+                            df_trajectory_error.to_csv(path_or_buf=new_json_save_path + '/trajectory_error.csv', sep='\t', index=False)
+                            df_whole_error.to_csv(path_or_buf=new_json_save_path + '/whole_error.csv', sep='\t', index=False)
+
+            # print number of all models with correct state trajectories and display running time
+            print('Amount of models with correct state trajectories: ' + str(counter))
+            print('time needed: ' + str(time.time() - start_time))
+
+
+# call function
+compStaTraj_JWS_2()
---
+++ compareStatetrajectories_140 settings_1.py
@@ -0,0 +1,262 @@
+# script 1 to compare state trajectories from JWS with trajectories of the AMICI simulation
+# => creates two important .tsv files
+
+# Attention:    boundary conditions are not being simulated by JWS!
+
+from execute_loadModels import *
+from JWS_changeValues import *
+import amici.plotting
+import numpy as np
+import libsbml
+import libsedml
+import pandas as pd
+import os
+import urllib.request
+import requests
+import json
+import itertools
+import time
+import shutil
+
+
+# create folder with all models that did not pass the trajectory check for the best setting 2_2_9_12_10
+path_all_models = '../sbml2amici/amici_models_newest_version_0.10.19'
+path_correct_trajectories = '../sbml2amici/correct_amici_models_paper'
+path_not_correct_trajectories = '../sbml2amici/0.10.19_without_correct'
+if not os.path.exists(path_not_correct_trajectories):
+    amici = sorted(os.listdir(path_all_models))
+    correct = sorted(os.listdir(path_not_correct_trajectories))
+    for iModel in amici:
+        if iModel not in correct:
+            shutil.copytree(path_all_models + '/' + iModel, path_not_correct_trajectories + '/' + iModel)
+        else:
+            model_amici = sorted(os.listdir(path_all_models + '/' + iModel))
+            model_correct = sorted(os.listdir(path_correct_trajectories + '/' + iModel))
+            for iFile in model_amici:
+                if iFile not in model_correct:
+                    if not os.path.exists(path_not_correct_trajectories + '/' + iModel):
+                        os.makedirs(path_not_correct_trajectories + '/' + iModel)
+                    shutil.copytree(path_all_models + '/' + iModel + '/' + iFile, path_not_correct_trajectories + '/' + iModel + '/' + iFile)
+                else:
+                    continue
+
+
+def compStaTraj_JWS(delete_counter, Algorithm, Iterative, Linear, Tolerances):
+    # create new folder for all json files
+    folder_path = './Whole_state_trajectory_comparison'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    #if not os.path.exists('./json_folder'):
+    #    os.makedirs('./json_folder')
+
+    # set settings for simulation
+    algorithm = [2]#[1, 2]
+    if Algorithm != '':
+        Index_correct_algorithm = algorithm.index(Algorithm)
+        algorithm = algorithm[Index_correct_algorithm:]
+    for solAlg in algorithm:
+
+        iterative = [1, 2]
+        if Iterative != '':
+            Index_correct_nonlinsol = iterative.index(Iterative)
+            iterative = iterative[Index_correct_nonlinsol:]
+        for nonLinSol in iterative:
+
+            linearsol = [1, 6, 7, 8, 9]
+            if Linear != '':
+                Index_correct_linearsol = linearsol.index(Linear)
+                linearsol = linearsol[Index_correct_linearsol:]
+            for linSol in linearsol:
+
+                Tolerance_combination = [[1e-6, 1e-8], [1e-8, 1e-6], [1e-8, 1e-16], [1e-16, 1e-8],
+                                         [1e-10, 1e-12], [1e-12, 1e-10], [1e-14, 1e-14]]
+                if Tolerances != '':
+                    Index_correct_tolerances = Tolerance_combination.index(Tolerances)
+                    Tolerance_combination = Tolerance_combination[Index_correct_tolerances :]
+                for iTolerance in Tolerance_combination:
+                    start = time.time()
+
+                    # split atol and rtol for naming purposes
+                    _,atol_exp = str('{:.1e}'.format(iTolerance[0])).split('-')
+                    _,rtol_exp = str('{:.1e}'.format(iTolerance[1])).split('-')
+                    if len(atol_exp) != 2:
+                        atol_exp = '0' + atol_exp
+                    if len(rtol_exp) != 2:
+                        rtol_exp = '0' + rtol_exp
+
+                    # get name of jws reference
+                    url = "https://jjj.bio.vu.nl/rest/models/?format=json"
+                    view_source = requests.get(url)
+                    json_string = view_source.text
+                    json_dictionary = json.loads(json_string)
+
+                    # get all models
+                    list_directory_amici = sorted(os.listdir('../sbml2amici/0.10.19_without_correct'))
+                    if delete_counter != 0:
+                        del list_directory_amici[0:delete_counter]
+
+                    for iMod in range(0, len(list_directory_amici)):
+
+                        iModel = list_directory_amici[iMod]
+                        list_files = sorted(os.listdir('../sbml2amici/0.10.19_without_correct/' + iModel))
+
+                        for iFile in list_files:
+
+                            # important paths
+                            json_save_path = folder_path + '/' + f'{solAlg}_{nonLinSol}_{linSol}_{atol_exp}_{rtol_exp}' \
+                                             + '/json_files/' + iModel + '/' + iFile
+                            sedml_path = './sedml_models/' + iModel + '/' + iModel + '.sedml'
+                            sbml_path = './sedml_models/' + iModel + '/sbml_models/' + iFile + '.sbml'
+                            BioModels_path = './BioModelsDatabase_models'
+
+
+                            if os.path.exists(BioModels_path + '/' + iModel):
+                                print('Model is not part of JWS-database!')
+                            else:
+                                # Open SBML file
+                                sbml_model = libsbml.readSBML(sbml_path)
+
+                                # get right model reference from sbml model
+                                parse_name_model = sbml_model.getModel().getId()
+                                for iCount in range(0, len(json_dictionary)):
+                                    parse_name_jws = json_dictionary[iCount]['slug']
+                                    if parse_name_model == parse_name_jws:
+                                        model_reference = json_dictionary[iCount]['slug']
+                                        break
+                                # elements in json_dictionary are only lower case --- the sbml model has upper case models
+                                try:
+                                    model_reference
+                                except:
+                                    wrong_model_name = ["".join(x) for _, x in itertools.groupby(parse_name_model,
+                                                                                                 key=str.isdigit)]
+                                    if wrong_model_name[0].islower() == False:
+                                        correct_model_letters = wrong_model_name[0].lower()
+                                        correct_model_name = correct_model_letters + wrong_model_name[1]
+                                        for iCount in range(0, len(json_dictionary)):
+                                            parse_name_jws = json_dictionary[iCount]['slug']
+                                            if correct_model_name == parse_name_jws:
+                                                model_reference = json_dictionary[iCount]['slug']
+                                                break
+                                # check if 'all_settings' works
+                                try:
+                                    # Get whole model
+                                    model = all_settings(iModel, iFile)
+
+                                    # create folder
+                                    if not os.path.exists(json_save_path):
+                                        os.makedirs(json_save_path)
+                                except:
+                                    print('Model ' + iModel + ' extension is missing!')
+                                    continue
+
+
+                                ######### jws simulation
+                                # Get time data with num_time_points == 100
+                                t_data = model.getTimepoints()
+                                sim_start_time = t_data[0]
+                                sim_end_time = t_data[len(t_data) - 1]
+                                sim_num_time_points = 101
+                                model.setTimepoints(np.linspace(sim_start_time, sim_end_time, sim_num_time_points))
+
+                                # Open sedml file
+                                sedml_model = libsedml.readSedML(sedml_path)
+
+                                # import all changes from SEDML
+                                list_of_strings = JWS_changeValues(iFile, sedml_model)
+
+                                # Get Url with all changes
+                                # <species 1>=<amount>
+                                # <parameter 1>=<value>, compartment == parameter (in this case)
+                                url = 'https://jjj.bio.vu.nl/rest/models/' + model_reference + '/time_evolution?time_end=' + \
+                                      str(sim_end_time) + ';species=all;'
+
+                                for iStr in list_of_strings:
+                                    url = url + iStr
+
+                                #### Save .json file
+                                urllib.request.urlretrieve(url, json_save_path + '/' + iFile + '_JWS_simulation.json')
+
+                                #### write as .csv file
+                                json_2_csv = pd.read_json(json_save_path + '/' + iFile + '_JWS_simulation.json')
+                                json_2_csv.to_csv(json_save_path + '/' + iFile + '_JWS_simulation.csv', sep='\t', index=False)
+
+                                # open new .csv file
+                                tsv_file = pd.read_csv(json_save_path + '/' + iFile + '_JWS_simulation.csv', sep='\t')
+
+                                # columns names of .tsv file
+                                column_names = list(tsv_file.columns)
+                                column_names.remove('time')
+                                del tsv_file['time']
+
+                                ########## model simulation
+                                # Create solver instance
+                                solver = model.getSolver()
+
+                                # set all settings
+                                solver.setAbsoluteTolerance(iTolerance[0])
+                                solver.setRelativeTolerance(iTolerance[1])
+                                solver.setLinearSolver(linSol)
+                                solver.setNonlinearSolverIteration(nonLinSol)
+                                solver.setLinearMultistepMethod(solAlg)
+
+                                # set stability flag for Adams-Moulton
+                                if solAlg == 1:
+                                    solver.setStabilityLimitFlag(False)
+
+                                # Simulate model
+                                sim_data = amici.runAmiciSimulation(model, solver)
+
+                                # print some values
+                                for key, value in sim_data.items():
+                                    print('%12s: ' % key, value)
+
+                                # Get state trajectory
+                                state_trajectory = sim_data['x']
+
+                                # Delete all trajectories for boundary conditions
+                                delete_counter = 0
+                                all_properties = sbml_model.getModel()
+                                for iSpec in range(0, all_properties.getNumSpecies()):
+                                    all_species = all_properties.getSpecies(iSpec)
+                                    if all_species.getBoundaryCondition() == True:
+                                        state_trajectory = state_trajectory.transpose()
+                                        if delete_counter == 0:
+                                            state_trajectory = np.delete(state_trajectory, iSpec, 0)
+                                        else:
+                                            state_trajectory = np.delete(state_trajectory, iSpec - delete_counter, 0)
+                                        state_trajectory = state_trajectory.transpose()
+                                        delete_counter = delete_counter + 1
+
+                                # Convert ndarray 'state-trajectory' to data frame and save it
+                                try:
+                                    df_state_trajectory = pd.DataFrame(columns=column_names, data=state_trajectory)
+                                except:
+                                    print('Try again for model ' + list_directory_amici[iMod] + '_' + iFile)
+                                    compStaTraj_JWS(iMod, solAlg, nonLinSol, linSol, iTolerance)
+                                df_state_trajectory.to_csv(json_save_path + '/' + iFile + '_model_simulation.csv', sep='\t')
+                    end = time.time()
+                    print(end - start)
+
+                # reset values - linear solver
+                Algorithm = ''
+                Iterative = ''
+                Linear = ''
+                Tolerances = ''
+
+            # reset values - nonlinear solver
+            Algorithm = ''
+            Iterative = ''
+            Linear = ''
+            Tolerances = ''
+
+        # reset values - solver algorithm
+        Algorithm = ''
+        Iterative = ''
+        Linear = ''
+        Tolerances = ''
+
+    print('All combinations have been satisfied!')
+    sys.exit()
+
+# call function, starting with no models to delete
+compStaTraj_JWS(0, '', '', '', '')
